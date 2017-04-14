5f9a6ea1fc0186516a65e292639263db32fcd240
Krzysiek Greń <k.gren@selleo.com>
2017-04-14 15:37:02 +0200

    added devise_token_auth, try to setup on client side

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   jsonapi_resources :categories
   jsonapi_resources :posts
