dff5fef9e425d326f740323a953b8049c1dce48a
tb <t.bak@selleo.com>
2017-04-15 18:03:22 +0200

    react-md layout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { Component } from 'react';
+import IndexLink from 'react-router/lib/IndexLink';
+import NavigationDrawer from 'react-md/lib/NavigationDrawers';
+import FontIcon from 'react-md/lib/FontIcons';
 
-const App = (props) => (
-  <div>
-    <p>
-      <Link to={'/posts'}>Posts</Link> |&nbsp;
-      <Link to={'/categories'}>Categories</Link>
-    </p>
-    <hr />
-    {props.children}
-  </div>
-);
+class App extends Component {
+  render() {
+    const { children } = this.props;
+
+    const navItems = [
+      {
+        key: 'posts',
+        primaryText: 'Posts',
+        leftIcon: <FontIcon>language</FontIcon>,
+        component: IndexLink,
+        to: '/posts',
+      },
+      {
+        key: 'categories',
+        primaryText: 'Categories',
+        leftIcon: <FontIcon>filter_list</FontIcon>,
+        component: IndexLink,
+        to: '/categories',
+      },
+      {
+        key: 'logout',
+        primaryText: 'Logout',
+        leftIcon: <FontIcon>exit_to_app</FontIcon>,
+      },
+    ];
+
+    return (
+      <div>
+        <NavigationDrawer
+          navItems={navItems}
+          mobileDrawerType={NavigationDrawer.DrawerTypes.TEMPORARY}
+          tabletDrawerType={NavigationDrawer.DrawerTypes.TEMPORARY}
+          desktopDrawerType={NavigationDrawer.DrawerTypes.FULL_HEIGHT}
+        >
+          <section className="md-grid md-grid--40-16">
+            <section className="main-content md-cell md-cell--12">
+              {children}
+            </section>
+          </section>
+        </NavigationDrawer>
+      </div>
+    );
+  }
+};
 
 export default App;
